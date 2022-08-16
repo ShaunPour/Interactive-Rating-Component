255de07a0afabed10cb7fea42574de8a7be3fd3e
Shaun <scpour@gmail.com>
2022-08-16 09:52:00 -0400

    1.25

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 const form = document.querySelector('form');
 const thanks = document.querySelector('.thank-you');
 const submit = document.getElementById('submit');
-const rating = document.querySelector('rating-value');
+const rating = document.querySelector('.rating-value');
+const ratingBtn = document.querySelectorAll('form button');
+let score = 3;
 
-submit.addEventListener('click', function(e) {
+submit.addEventListener('click', onSubmit);
+
+ratingBtn.forEach(btn => {
+    btn.addEventListener('click', handleRatingBtnClick);
+});
+
+function onSubmit(e) {
     e.preventDefault();
-    thanks.classList.remove('hide');
+    rating.innerHTML = score;
     thanks.classList.add('show');
-})
+    thanks.classList.remove('hide');
+}
+
+function handleRatingBtnClick(event) {
+    event.preventDefault();
+    if(event.target.classList.contains('rating-btn')) {
+    score = event.target.innerHTML;
+    console.log(score);
+    }
+}
