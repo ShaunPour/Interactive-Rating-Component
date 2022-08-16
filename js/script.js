ac0c2e1754f3c9c62aa8704ed27e5b647e920844
Shaun <scpour@gmail.com>
2022-08-15 20:07:19 -0400

    1.21

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const thanks = document.querySelector('.thank-you');
 const submit = document.getElementById('submit');
+const rating = document.querySelector('rating-value');
 
 submit.addEventListener('click', function(e) {
     e.preventDefault();
